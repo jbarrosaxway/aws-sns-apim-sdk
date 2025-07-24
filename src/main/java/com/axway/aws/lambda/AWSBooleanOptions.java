7825950b2568a59b2a0178a97386e39eaec3ed9c
João Barros <jbarros@axway.com>
2025-07-24 00:02:49 -0300

    fix: replace CheckboxAttribute with ComboAttribute for useIAMRole field
    - Replace non-working CheckboxAttribute with ComboAttribute
    - Add AWSBooleanOptions utility class to provide boolean values
    - Use Yes/No options instead of checkbox for better compatibility
    - Maintain same functionality with improved UI compatibility

---
+++ src/main/java/com/axway/aws/lambda/AWSBooleanOptions.java
@@ -0,0 +1,21 @@
+package com.axway.aws.lambda;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Utility class to provide boolean options for ComboAttribute
+ */
+public class AWSBooleanOptions {
+    
+    /**
+     * Provides boolean options for UI components
+     * @return Map with boolean options
+     */
+    public static Map<String, String> booleanOptions() {
+        Map<String, String> options = new HashMap<>();
+        options.put("true", "Yes");
+        options.put("false", "No");
+        return options;
+    }
+} 
